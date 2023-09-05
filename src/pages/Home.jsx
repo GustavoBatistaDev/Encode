400f1f9f30c58749f828c4c20121ef75e58f7056
GustavoBatistaDev <gustavobatistadev@gmail.com>
2023-09-05 01:08:04 -0300

    adicionando section de services

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import logo from '../assets/Logotipo icon 1.png';
 import logo2 from '../assets/Logotipo 2.png';
+import rocket from '../assets/rocket.svg';
+import arrow from '../assets/arrow.svg';
 import monitor from '../assets/monitorNovo.png';
 import portifolio from '../assets/5adc69c317a82e88002b217727f36257.jpg';
 import { useInView } from 'react-intersection-observer';
@@ -46,14 +48,14 @@ export const Home = () => {
                                         <a href="#">
                                             <i className="fa-brands fa-instagram" />
                                         </a>
-                                        <a  href="https://web.whatsapp.com/send?phone=5573981933667">
+                                        <a href="https://web.whatsapp.com/send?phone=5573981933667">
                                             <i className="fa-brands fa-whatsapp" />
                                         </a>
                                         <a href="#">
                                             <i className="fa-brands fa-behance" />
                                         </a>
                                     </div>
-                                    <a  
+                                    <a
                                         target="_blank"
                                         className="ml-auto"
                                         href="https://web.whatsapp.com/send?phone=5573981933667" rel="noreferrer"
@@ -77,17 +79,32 @@ export const Home = () => {
                                     Desenvolvemos sites cativantes para elevar a sua marca ao próximo
                                     nível.
                                 </p>
-                               
+
                                 <a href="https://web.whatsapp.com/send?phone=5573981933667">
-                                            <button className="btn-header2">VER TODOS OS SERVIÇOS</button>
-                                        </a>
+                                    <button className="btn-header2">VER TODOS OS SERVIÇOS</button>
+                                </a>
 
                             </div>
-                            
+
                             <div ref={ref3} className={`header-img col-md-6  fade-in ${inView3 ? 'show' : ''}`} >
                                 <img src={monitor} />
                             </div>
                         </div>
+
+                    </div>
+                    <div className="fixed-network">
+                        <a href="#" className='d-block text-white' target="_blank">
+                            <i className="fa-brands fa-linkedin-in" />
+                        </a>
+                        <a href="#" className='d-block text-white'>
+                            <i className="fa-brands fa-instagram" />
+                        </a>
+                        <a href="https://web.whatsapp.com/send?phone=5573981933667" className='d-block text-white'>
+                            <i className="fa-brands fa-whatsapp" />
+                        </a>
+                        <a href="#" className='d-block text-white'>
+                            <i className="fa-brands fa-behance" />
+                        </a>
                     </div>
                 </header>
                 {/* carrosel */}
@@ -129,53 +146,185 @@ export const Home = () => {
                 <section className="section1">
                     <div className="container">
                         <div>
-                            <div ref={ref4} className={`text-center col-md-12  fade-in ${inView4 ? 'show' : ''}`}>
-                                <p className="p-section1 mb-5">
-                                    <span className="section1-nome">ENCODE</span>
-                                    <br />
-                                    Desenvolvendo a solução que o seu negócio{" "}
-                                    <span
-                                        style={{
-                                            background: "linear-gradient(to right ,#03a9f4, #FF7DB2)",
-                                            backgroundClip: "text",
-                                            WebkitBackgroundClip: "text",
-                                            color: "black",
-                                            WebkitTextFillColor: "transparent",
-                                            fontSize: "1.2em",
-                                            fontWeight: 600
-                                        }}
-                                    >
-                                        precisa!
-                                    </span>
-                                </p>
-                            </div>
+
                             <div ref={ref5} className={`row d-flex justify-content-center fade-in ${inView5 ? 'show' : ''}`} >
                                 <div className="col-md-6">
-                                    <h1 className="method text-left"> Nossa Metodologia</h1>
+                                    <h1 style={{ fontSize: '50px' }} className="text-white text-left">Como <span className='method'>Nós</span> fazemos?</h1>
                                     <div className="box-tec text-left">
-                                        <h1 className="work">Como trabalhamos?</h1>
-                                        <p> 
+
+                                        <p >
                                             Primeiro desenvolvemos a sua identidade visual e, com base nela,
                                             criamos a sua interface de usuário (UI). Em seguida,
                                             personalizamos o site para que ele reflita perfeitamente a sua
-                                            identidade e estilo.
+                                            identidade e estilo. Um site moderno e otumizado é essencial para as empresas. Com velocidade de carregamento elevada, site responsivo e uma ótima experiência de usuário, a empresa atrai e retém clientes. Além disso, a otimização no Google melhora a visibilidade nos resultados de pesquisa, impulsionando o crescimento e o sucesso do negócio.
                                         </p>
-                                        <a  href="https://api.whatsapp.com/send?phone=5573981933667&text=Oi,%20gostaria%20de%20mais%20informações%20sobre%20a%20Encode.%20">
+                                        <a href="https://api.whatsapp.com/send?phone=5573981933667&text=Oi,%20gostaria%20de%20mais%20informações%20sobre%20a%20Encode.%20">
                                             <button className="btn-header2">VER TODOS OS SERVIÇOS</button>
                                         </a>
                                     </div>
                                 </div>
-                                <div id="sd" className="col-md-6">
-                                    <img
-                                        className="foguete"
-                                        src="https://static.wixstatic.com/media/059206_096e9b8ec2874b1bb7d412673d8a0e00~mv2.png/v1/crop/x_209,y_0,w_679,h_1080/fill/w_406,h_642,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Foguete-8-to-onePrancheta-1.png"
-                                        alt=""
-                                    />
+                                <div className="col-md-6">
+                                    <div className="container-images d-flex">
+                                        <img className='mx-2' src={rocket} alt="" />
+                                        <img className='mx-2' src={rocket} alt="" />
+                                    </div>
+                                    <div className="container-images d-flex">
+                                        <img className='mx-2' src={rocket} alt="" />
+                                        <img className='mx-2' src={rocket} alt="" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </section>
+
+
+
+                <section className="section1">
+                    
+                    <div className="container-fluid">
+                    <div className="row justify-content-center">
+                        <div className="">
+                            <div className="card-service ">
+                                <div className="container-header">
+                                    <h2>Site</h2>
+                                    <h1 className='services-title'>Institucional</h1>
+                                </div>
+                                <div className="services-content">
+                                    <ul>
+                                        
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Responsivo - Celular, PC & Tablet;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Com até 5 páginas;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Tenha seu blog;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Ofereça seus serviços;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Capte clientes 24h por dia;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Criação de email comercial;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Ideal para autônomos/empresas;
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            
+
+                        </div>
+                        <div className="">
+                        <div className="card-service">
+                                <div className="container-header">
+                                    <h2>Site</h2>
+                                    <h1 className='services-title'>Vendas</h1>
+                                </div>
+                                <div className="services-content">
+                                    <ul>
+                                        
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Responsivo - Celular, PC & Tablet;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Carregamento rápido;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Copywriting profissional;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Design moderno & criativo;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Integração c/ ferramentas de  &nbsp;  &nbsp;marketing;
+                                        </li>
+                                       
+                                    </ul>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div className="">
+                        <div className="card-service">
+                                <div className="container-header">
+                                    <h2>Site</h2>
+                                    <h1 className='services-title'>Landing Pages</h1>
+                                </div>
+                                <div className="services-content">
+                                    <ul>
+                                        
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Responsivo - Celular, PC & Tablet;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Integração com WhatsApp;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Aumente as vendas com Up-sells;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp; Down-sells e Cross-sells;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Criação de e-mail comercial;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Criação de email comercial;
+                                        </li>
+                                        <li className='my-3 text-left'>
+                                            <img src={arrow} alt="" />
+                                            &nbsp;Carregamento rápido;
+                                        </li>
+                                       
+                                    </ul>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    </div>
+                </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 <section className="section2">
                     <div className="container">
                         <div ref={ref6} className={` text-center fade-in ${inView6 ? 'show' : ''}`}>
@@ -203,9 +352,9 @@ export const Home = () => {
                     </div>
                     {/* Carrosel web */}
                     <div
-                    ref={ref8} className={`carousel slide fade-in ${inView8 ? 'show' : ''}`}
+                        ref={ref8} className={`carousel slide fade-in ${inView8 ? 'show' : ''}`}
                         id="carouselExampleControls"
-                        
+
                         data-ride="carousel"
                     >
                         <div className="carousel-inner">
@@ -388,7 +537,7 @@ export const Home = () => {
                     {/* Carrosel celular */}
                 </section>
 
-                <section  ref={ref9} className={`section3 fade-in ${inView9 ? 'show' : ''}`} >
+                <section ref={ref9} className={`section3 fade-in ${inView9 ? 'show' : ''}`} >
                     <div className="container">
                         <div className="text-center mb-5">
                             <img src="media/Logotipo icon 1.png" className="mb-3" alt="" />
@@ -477,6 +626,5 @@ export const Home = () => {
             </>
 
         </>
-
     );
 };
