fe1519eba4b303dcf81736ef3dfcee47f1640de9
GustavoBatistaDev <gustavobatistadev@gmail.com>
2023-09-21 02:28:28 -0300

    ajustando

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,14 +1,14 @@
 // App.js
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Loader } from './pages/loader';
+
 import { Home } from './pages/Home';
 
 export function MyRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Loader />} />
-        <Route path="/home" element={<Home />} />
+      
+        <Route path="/" element={<Home />} />
         
         
       </Routes>
